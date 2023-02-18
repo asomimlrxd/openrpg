a0aa14ad9f41ab99763fe0b4e699806ea970a6a9
asomimlrxd <68653019+asomimlrxd@users.noreply.github.com>
2023-02-18 22:25:47 +0000

    Update 1.tsx

--- src/pages/sheet/player/1.tsx
+++ src/pages/sheet/player/1.tsx
@@ -112,7 +112,7 @@ function PlayerSheet(props: PageProps) {
 						</Row>
 						<hr />
 						<Row className='mb-3'>
-							<DataContainer outline title='Abilidades'>
+							<DataContainer outline title='Habilidades'>
 								<PlayerCharacteristicContainer
 									playerCharacteristics={chars}
 									characteristicDiceConfig={
